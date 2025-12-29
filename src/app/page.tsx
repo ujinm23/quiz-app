191909bed52a40bd3d266f810ff3b80f626bac4d
Ujin <ujin@Ujins-MacBook-Pro.local>
2025-12-30 00:25:47 +0800

    qqqaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Header from "./_features/header";
-import { AppSidebar } from "./_features/appSideBar";
+"use client";
 import Step1 from "./content/Step1";
 import AppShell from "./_features/AppShell";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const getData = async() => {
+    try{
+    const data = await fetch("/api/routes/article");
+    console.log(data);
+  } catch(err){
+    console.log(err);
+  }
+}
+  useEffect (()=>{
+    getData()
+  }, []);
+
   return (
     <AppShell>
       <div className="flex flex-col w-screen h-auto">
